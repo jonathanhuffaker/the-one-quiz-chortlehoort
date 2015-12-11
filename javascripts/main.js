54d047e98d13361b3af1aa1bfd6ff1584972524a
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-12-10 23:04:24 -0600

    finished array portion.  completing remainder tomorrow

--- javascripts/main.js
+++ javascripts/main.js
@@ -33,7 +33,43 @@ function subtract(int1, int2) {
 
 doMath (100, 25, add);
 doMath (100, 25, subtract);
-
 console.log(doMath (100, 25, add));
 console.log(doMath (100, 25, subtract));
 
+// =========================
+
+// ARRAYS PART OF QUIZ
+// Write a function named sortedPlanets that returns an array of the planet names sorted alphabetically.
+
+function sortedPlanets(Planets) {
+	return Planets.sort();
+}
+
+// console.log(sortedPlanets(Planets));
+
+// Write a function named reversedPlanets that returns an array of planet names that are reversed (i.e. "htraE").
+
+function reversedPlanets(Planets) {
+	var reversedPlanetsArray = [];
+	for (var i=0; i<Planets.length; i++) {
+		reversedPlanetsArray.push(Planets[i].split("").reverse().join(""))
+	}
+	return reversedPlanetsArray
+}
+
+// console.log(reversedPlanets(Planets));
+
+// Write a function named longPlanets that returns an array containing only the planet names that are 7 characters long, or longer.
+
+function longPlanets() {
+	var longerNamedPlanetsArray = [];
+	for (var i=0; i<Planets.length; i++) {
+		if (Planets[i].charAt(6) !=="") {
+			longerNamedPlanetsArray.push(Planets[i]);
+		}
+	}
+		return longerNamedPlanetsArray
+
+}
+
+// console.log(longPlanets(Planets));
